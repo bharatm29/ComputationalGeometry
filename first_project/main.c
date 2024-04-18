98a2cd86b5c03a893c176108de027389a2a7cfe8
bharatm29 <bharatmhes@gmail.com>
2024-04-18 17:31:06 +0530

    title screen almost done

--- first_project/main.c
+++ first_project/main.c
@@ -4,6 +4,8 @@
 #include "../raygui/raygui.h"
 #include <stdbool.h>
 
+typedef enum { TITLE, BUBBLE_SORT, SELECTION_SORT, INSERTION_SORT } State;
+
 void renderArray(const int arr[], const int size) {
     for (int i = 0; i < size; i++) {
 
@@ -15,7 +17,7 @@ void renderArray(const int arr[], const int size) {
         const int posX = 35 * i + 25;
 
         // 25px gap from the top
-        const int posY = 0 + 25;
+        const int posY = 0 + 25 + 15;
 
         // each rect is of 20px width
         const int width = 20;
@@ -41,21 +43,92 @@ int main() {
     bool sorted = false;
     bool render = false;
     bool btnExportPressed = false;
+    State currentState = TITLE;
     int framCounter = -1;
     int waitTime = 60;
 
+    int selectedSortTech = 0;
+
+    const char *sortingTechs[] = {"Bubble Sort", "Selection Sort",
+                                  "Insertion Sort"};
+
     while (!WindowShouldClose()) {
-        BeginDrawing();
-
-        ClearBackground(WHITE);
-
-        DrawText("Bubble Sort", 10, 5, 20, RED);
-
-        if (IsKeyPressed(KEY_ENTER)) {
-            render = true;
+        if (render) {
+            switch (selectedSortTech) {
+            case 0: {
+                currentState = BUBBLE_SORT;
+            } break;
+            case 1: {
+                currentState = SELECTION_SORT;
+            } break;
+            case 2: {
+                currentState = INSERTION_SORT;
+            } break;
+            default: {
+                currentState = TITLE;
+            } break;
+            }
         }
 
-        if (render) {
+        BeginDrawing();
+        GuiEnable();
+
+        switch (currentState) {
+        case TITLE: {
+            ClearBackground(SKYBLUE);
+
+            const char title[] = {"Sortualizer"};
+
+            int fontSize = 20;
+            int textWidth = MeasureText(title, fontSize);
+
+            DrawText(title, (WIDTH - textWidth) / 2, 15, fontSize, BLUE);
+
+            const char selectText[] = "Pick a sorting technique";
+
+            fontSize = 20;
+            textWidth = MeasureText(selectText, fontSize);
+
+            DrawText(selectText, (WIDTH - textWidth) / 2, HEIGHT / 2 - 60,
+                     fontSize, BLUE);
+
+            // Extract Into Varible
+            const int defaultFontSize = 15;
+            GuiSetStyle(DEFAULT, TEXT_SIZE, defaultFontSize);
+
+            const int comboBoxWidth =
+                // FIXME: Make the longest word dynamic and figure out what's
+                // missing in the calculation :(
+                MeasureText("Selection Sort", defaultFontSize) +
+                GuiGetStyle(COMBOBOX, COMBO_BUTTON_WIDTH) +
+                GuiGetStyle(COMBOBOX, COMBO_BUTTON_SPACING) + defaultFontSize;
+
+            GuiComboBox((Rectangle){(WIDTH - comboBoxWidth) / 2.0, HEIGHT / 2.0 - 15,
+                                    comboBoxWidth, 30},
+                        TextJoin(sortingTechs,
+                                 sizeof(sortingTechs) / sizeof(sortingTechs[0]),
+                                 ";"),
+                        &selectedSortTech);
+
+            const char btnTxt[] = "Sort!";
+            const int sortButtonWidth = 10 + MeasureText(btnTxt, defaultFontSize) + 10;
+
+            const bool isSortButtonPressed =
+                GuiButton((Rectangle){(WIDTH - sortButtonWidth) / 2.0,
+                                      HEIGHT / 2.0 + 25, sortButtonWidth, 30},
+                          btnTxt);
+
+            if (IsKeyPressed(KEY_ENTER) || isSortButtonPressed) {
+                render = true;
+            }
+        } break;
+
+        case BUBBLE_SORT: {
+
+            ClearBackground(WHITE);
+
+            DrawText("Bubble Sort", 10, 5, 20, RED);
+
             if (!sorted && framCounter == waitTime) {
                 framCounter = 0;
                 if (i < size) {
@@ -77,6 +150,19 @@ int main() {
 
                 framCounter++;
             }
+        } break;
+        case SELECTION_SORT: {
+            ClearBackground(WHITE);
+            DrawText("Selection Sort", 10, 5, 20, RED);
+        } break;
+        case INSERTION_SORT: {
+            ClearBackground(WHITE);
+            DrawText("Insertion Sort", 10, 5, 20, RED);
+        } break;
+        default: {
+            ClearBackground(WHITE);
+            DrawText("Illegal State pls fix quick", 10, 5, 20, RED);
+        } break;
         }
 
         EndDrawing();
