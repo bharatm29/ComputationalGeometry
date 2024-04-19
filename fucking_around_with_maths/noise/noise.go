11fde8a2d4de772a9f15e786dabf1620da4650c4
bharatm29 <bharatmhes@gmail.com>
2024-04-19 23:15:12 +0530

    drawing vectors now onto the particle system

--- fucking_around_with_maths/noise/noise.go
+++ fucking_around_with_maths/noise/noise.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
 
 	"github.com/aquilax/go-perlin"
@@ -8,8 +10,8 @@ import (
 )
 
 const (
-	WIDTH  int32 = 600
-	HEIGHT int32 = 600
+	WIDTH  int32 = 800
+	HEIGHT int32 = 800
 )
 
 func main() {
@@ -22,30 +24,51 @@ func main() {
 	noise := perlin.NewPerlinRandSource(2, 2, 4, rand.NewSource(rand.Int63()))
 
 	inc := 0.01
-    start := 0.0
+	start := 0.0
+
+	scale := int32(10)
+
+	cols := WIDTH / scale
+	rows := HEIGHT / scale
 
 	for !rl.WindowShouldClose() {
 
 		rl.BeginDrawing()
 
 		{
-			rl.ClearBackground(rl.Black)
+			rl.ClearBackground(rl.White)
 
 			yoff := start
-			for y := int32(0); y < HEIGHT; y++ {
+			for y := int32(0); y < rows; y++ {
 				xoff := start
-				for x := int32(0); x < WIDTH; x++ {
-					mappedNoise := ((noise.Noise2D(xoff, yoff) + 1) / 2) // converting [-1, 1] to [0, 1]
+				for x := int32(0); x < cols; x++ {
+                    mappedNoise := ((noise.Noise2D(xoff, yoff) + 1) / 2)
 					r := uint8(mappedNoise * 255)
-					rl.DrawPixel(x, y, rl.Color{R: r, B: r, G: r, A: 255})
-                    xoff += inc
+					xoff += inc
+
+					_ = rl.Color{R: r, G: r, B: r, A: r}
+
+                    v := vectorFromAngle(rl.Pi * mappedNoise) // Create a vector from the angle
+                    v = rl.Vector2Scale(v, float32(scale))
+
+                    // Draw the line
+                    rl.DrawLineV(rl.Vector2{X: float32(x * scale), Y: float32(y * scale)}, rl.Vector2{X: float32(x*scale) + v.X, Y: float32(y*scale) + v.Y}, rl.Black)
+
+					// rl.DrawRectangle(x*scale, y*scale, int32(scale), int32(scale), clr)
 				}
-                yoff += inc
+				yoff += inc
 			}
 
-            start += 0.01
+			rl.DrawText(fmt.Sprintf("%d", rl.GetFPS()), 20, 20, 10, rl.DarkBlue)
+			// start += 0.01
 		}
 
 		rl.EndDrawing()
 	}
 }
+
+
+func vectorFromAngle(angle float64) rl.Vector2 {
+    return rl.Vector2{X: float32(math.Cos(angle)), Y: float32(math.Sin(angle))}
+}
+
