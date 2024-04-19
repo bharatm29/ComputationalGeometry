74513f4ce1c9bb2f09c1f64289bfc2851f73309f
bharatm29 <bharatmhes@gmail.com>
2024-04-19 21:33:52 +0530

    visualized 2D perlin noise :)

--- fucking_around_with_maths/noise/noise.go
+++ fucking_around_with_maths/noise/noise.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
-	"slices"
 
 	"github.com/aquilax/go-perlin"
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -21,54 +19,31 @@ func main() {
 	rl.SetConfigFlags(rl.FlagMsaa4xHint) // Enables Anti-aliasing if available
 	rl.SetTargetFPS(60)
 
-	xoff := 0.0
-	xoff1 := 10000.0
-	noise := perlin.NewPerlinRandSource(2, 2, 3, rand.NewSource(rand.Int63()))
+	noise := perlin.NewPerlinRandSource(2, 2, 4, rand.NewSource(rand.Int63()))
 
-	start := 0.0
+	inc := 0.01
+    start := 0.0
 
 	for !rl.WindowShouldClose() {
+
 		rl.BeginDrawing()
 
 		{
 			rl.ClearBackground(rl.Black)
 
-			mappedNoise1 := ((noise.Noise1D(xoff) + 1) / 2) // converting [-1, 1] to [0, 1]
-			x := int32(mappedNoise1 * float64(WIDTH))
-
-			mappedNoise2 := ((noise.Noise1D(xoff1) + 1) / 2)
-			y := int32(mappedNoise2 * float64(HEIGHT))
-
-			xoff += 0.01
-			xoff1 += 0.02
-
-			rl.DrawText(fmt.Sprintf("(%d, %d)", x, y), 20, 20, 20, rl.Red)
-			rl.DrawLine(0, HEIGHT/2, WIDTH, HEIGHT/2, rl.DarkBlue)
-
-			var i int32
-
-			pathOff := start
-			path := []rl.Vector2{}
-
-			for i = 0; i < WIDTH; i++ {
-				mappedNoise2 := ((noise.Noise1D(pathOff) + 1) / 2)
-				y := int32(mappedNoise2 * float64(HEIGHT))
-
-				pos := rl.Vector2{X: float32(i), Y: float32(y)}
-
-				if !slices.Contains(path, pos) {
-					path = append(path, pos)
+			yoff := start
+			for y := int32(0); y < HEIGHT; y++ {
+				xoff := start
+				for x := int32(0); x < WIDTH; x++ {
+					mappedNoise := ((noise.Noise2D(xoff, yoff) + 1) / 2) // converting [-1, 1] to [0, 1]
+					r := uint8(mappedNoise * 255)
+					rl.DrawPixel(x, y, rl.Color{R: r, B: r, G: r, A: 255})
+                    xoff += inc
 				}
-
-				pathOff += 0.01
-
+                yoff += inc
 			}
 
-			start += 0.01
-
-			// rl.DrawCircle(x, y, 20, rl.Beige)
-
-			rl.DrawLineStrip(path, rl.Beige)
+            start += 0.01
 		}
 
 		rl.EndDrawing()
