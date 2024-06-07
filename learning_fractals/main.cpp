b6d8966e243953857a8fa8b36103e4172caf3811
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2024-06-07 18:00:00 +0530

    some refactoring. Brenley's fern done?!

--- learning_fractals/main.cpp
+++ learning_fractals/main.cpp
@@ -8,7 +8,13 @@ const int WIDTH = 800;
 const int HEIGHT = 800;
 const int ITERATIONS = 50000;
 
-// Define the affine transformations for the Barnsley Fern
+/*
+Define the affine transformations in the form: Ax + b
+[x'] = [a b][x] + [e]
+[y'] = [c d][y] + [f]
+
+The above is flatten to: [a, b, c, d, e, f] (row wise flattening)
+*/
 vector<vector<float>> transformations = {{0.0, 0.0, 0.0, 0.16, 0.0, 0.0},
                                          {0.2, -0.26, 0.23, 0.22, 0.0, 1.6},
                                          {-0.15, 0.28, 0.26, 0.24, 0.0, 0.44},
@@ -17,13 +23,12 @@ vector<vector<float>> transformations = {{0.0, 0.0, 0.0, 0.16, 0.0, 0.0},
 // Probabilities for each transformation
 vector<float> probabilities = {0.01, 0.07, 0.07, 0.85};
 
-// Apply the affine transformation to a point
 Vector2 applyTransformation(const vector<float> &transformation,
                             const Vector2 &point) {
     float x = transformation[0] * point.x + transformation[1] * point.y +
-              transformation[4];
+              transformation[4]; // ax + by + e
     float y = transformation[2] * point.x + transformation[3] * point.y +
-              transformation[5];
+              transformation[5]; // cx + dy + f
 
     return Vector2{x, y};
 }
@@ -31,15 +36,16 @@ Vector2 applyTransformation(const vector<float> &transformation,
 float gen_random_float(float min, float max) {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_real_distribution<float> dist6(.0, 1.);
+    std::uniform_real_distribution<float> dist(min, max);
 
-    return dist6(rng);
+    return dist(rng);
 }
 
 float mapRange(const float oldValue, const float oldMin, const float oldMax,
                const float newMin, const float newMax) {
     const float oldRange = (oldMax - oldMin);
     const float newRange = (newMax - newMin);
+
     const float newValue =
         (((oldValue - oldMin) * newRange) / oldRange) + newMin;
 
@@ -56,19 +62,28 @@ int main() {
     Vector2 point = {0, 0};
     points.push_back(point);
 
-    for (int i = 0; i < ITERATIONS; ++i) {
-        const float rnd = gen_random_float(.0, 1.);
+    for (int _ = 0; _ < ITERATIONS; ++_) {
+        // chose a random transformation using Inverse Transform Sampling
+        const float random_number = gen_random_float(.0, 1.);
+        float probabilitySum = 0.f;
 
-        if (rnd < 0.01) {
-            point = applyTransformation(transformations[0], point);
-        } else if (rnd < 0.08) {
-            point = applyTransformation(transformations[1], point);
-        } else if (rnd < 0.15) {
-            point = applyTransformation(transformations[2], point);
-        } else {
-            point = applyTransformation(transformations[3], point);
+        for (size_t index = 0; index < transformations.size(); index++) {
+            probabilitySum += probabilities[index];
+
+            // if the random number is the probability distribution range
+            if (random_number <= probabilitySum) {
+                point = applyTransformation(transformations[index], point);
+                break;
+            }
         }
+        /* Other method of picking the transformation according to the given
+         * probability:
+         * We can form a cumulative probability out of the given
+         * probability and then find the first equal or the next greater value
+         * to the random value, thus taking it's index.
+         */
 
+        // map the point into our WIDTH x HEIGHT coordinate space
         Vector2 mappedPoint = point;
 
         mappedPoint.x = mapRange(point.x, -2.1820, 2.6558, 0, WIDTH);
@@ -81,6 +96,7 @@ int main() {
         points.push_back(mappedPoint);
     }
 
+    // main loop
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
