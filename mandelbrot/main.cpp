f045d6affdcb0d2d31adf3b586cb5ab350402cd6
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2024-06-09 15:19:50 +0530

    now using shader but zoom doesn't work

--- mandelbrot/main.cpp
+++ mandelbrot/main.cpp
@@ -1,6 +1,5 @@
-#include <algorithm>
 #include <cmath>
-#include <cstdlib>
+#include <iostream>
 #include <raylib.h>
 #include <raymath.h>
 
@@ -9,157 +8,116 @@ const int HEIGHT = 800;
 
 const int ITERATIONS = 150;
 
-float mapRange(const float oldValue, const float oldMin, const float oldMax,
-               const float newMin, const float newMax) {
-    const float oldRange = (oldMax - oldMin);
-    const float newRange = (newMax - newMin);
-
-    const float newValue =
-        (((oldValue - oldMin) * newRange) / oldRange) + newMin;
-
-    return newValue;
-}
-
 int main() {
     InitWindow(WIDTH, HEIGHT, "MandelBrot Set");
 
     SetTargetFPS(60);
 
     // Zoomed in areas: [.4, .35], [-.55, -.5]
-    float sliderA = -2.f;  // should be negative
-    float sliderB = 1.45f; // should be positive
+    float minRange = -2.f;  // should be negative
+    float maxRange = 1.45f; // should be positive
 
     RenderTexture2D screen = LoadRenderTexture(WIDTH, HEIGHT);
 
-    Camera2D camera = { 0 };
+    Camera2D camera = {0};
     camera.zoom = 1.0f;
 
-    int zoomMode = 0;   // 0-Mouse Wheel, 1-Mouse Move
+    int zoomMode = 0; // 0-Mouse Wheel, 1-Mouse Move
 
-        BeginTextureMode(screen);
-        ClearBackground(BLACK);
-        {
-            for (int x = 0; x <= WIDTH; x++) {
-                for (int y = 0; y <= HEIGHT; y++) {
-                    float a = mapRange(x, 0, WIDTH, sliderA, sliderB);
-                    float b = mapRange(y, 0, HEIGHT, sliderA, sliderB);
+    Shader shader = LoadShader(0, "shader.glsl");
 
-                    const float ca = a;
-                    const float cb = b;
+    int resolutionLoc = GetShaderLocation(shader, "resolution");
+    int minRangeLoc = GetShaderLocation(shader, "minRange");
+    int maxRangeLoc = GetShaderLocation(shader, "maxRange");
+    int iterationLoc = GetShaderLocation(shader, "iterations");
 
-                    float counter{0};
+    float res[2] = {WIDTH, HEIGHT};
 
-                    while (counter < ITERATIONS) {
-                        const float aa = a * a - b * b;
-                        const float bb = 2 * a * b;
-
-                        a = aa + ca;
-                        b = bb + cb;
-
-                        counter++;
-
-                        if (std::abs(a + b) > 16) { // assuming 16 as infinity
-                            break;
-                        }
-                    }
-
-                    Color color = BLACK;
-
-                    if (counter < ITERATIONS) {
-                        double quotient = (double)counter / (double)ITERATIONS;
-                        double c = std::clamp<double>(quotient, 0.f, 1.f);
-
-                        c = mapRange(c, .0f, 1.f, 0, 255);
-
-                        if (quotient > 0.5) {
-                            // Close to the mandelbrot set the color changes
-                            // from green to white
-                            color = Color{(unsigned char)c, 255,
-                                          (unsigned char)c, 255};
-                        } else {
-                            // Far away it changes from black to green
-                            // color = GetColor(floatToHex(0, c, 0));
-                            color = Color{0, (unsigned char)c, 0, 255};
-                        }
-                    }
-
-                    DrawPixel(x, y, color);
-                }
-            }
-        }
-        EndTextureMode();
+    SetShaderValue(shader, resolutionLoc, res, SHADER_UNIFORM_VEC2);
+    SetShaderValue(shader, iterationLoc, (const void *)&ITERATIONS,
+                   SHADER_UNIFORM_INT);
 
     while (!WindowShouldClose()) {
         {
-        if (IsKeyPressed(KEY_ONE)) zoomMode = 0;
-        else if (IsKeyPressed(KEY_TWO)) zoomMode = 1;
+            if (IsKeyPressed(KEY_ONE))
+                zoomMode = 0;
+            else if (IsKeyPressed(KEY_TWO))
+                zoomMode = 1;
 
-        // Translate based on mouse right click
-        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-        {
-            Vector2 delta = GetMouseDelta();
-            delta = Vector2Scale(delta, -1.0f/camera.zoom);
-            camera.target = Vector2Add(camera.target, delta);
-        }
+            // Translate based on mouse right click
+            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                Vector2 delta = GetMouseDelta();
+                delta = Vector2Scale(delta, -1.0f / camera.zoom);
+                camera.target = Vector2Add(camera.target, delta);
+            }
 
-        if (zoomMode == 0)
-        {
-            // Zoom based on mouse wheel
-            float wheel = GetMouseWheelMove();
-            if (wheel != 0)
-            {
-                // Get the world point that is under the mouse
-                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+            if (zoomMode == 0) {
+                // Zoom based on mouse wheel
+                float wheel = GetMouseWheelMove();
+                if (wheel != 0) {
+                    // Get the world point that is under the mouse
+                    Vector2 mouseWorldPos =
+                        GetScreenToWorld2D(GetMousePosition(), camera);
 
-                // Set the offset to where the mouse is
-                camera.offset = GetMousePosition();
+                    // Set the offset to where the mouse is
+                    camera.offset = GetMousePosition();
 
-                // Set the target to match, so that the camera maps the world space point 
-                // under the cursor to the screen space point under the cursor at any zoom
-                camera.target = mouseWorldPos;
+                    // Set the target to match, so that the camera maps the
+                    // world space point under the cursor to the screen space
+                    // point under the cursor at any zoom
+                    camera.target = mouseWorldPos;
 
-                // Zoom increment
-                float scaleFactor = 1.0f + (0.25f*fabsf(wheel));
-                if (wheel < 0) scaleFactor = 1.0f/scaleFactor;
-                camera.zoom = Clamp(camera.zoom*scaleFactor, 0.125f, 64.0f);
+                    // Zoom increment
+                    float scaleFactor = 1.0f + (0.25f * fabsf(wheel));
+                    if (wheel < 0)
+                        scaleFactor = 1.0f / scaleFactor;
+                    camera.zoom =
+                        Clamp(camera.zoom * scaleFactor, 0.125f, 64.0f);
+                }
+            } else {
+                // Zoom based on mouse left click
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    // Get the world point that is under the mouse
+                    Vector2 mouseWorldPos =
+                        GetScreenToWorld2D(GetMousePosition(), camera);
+
+                    // Set the offset to where the mouse is
+                    camera.offset = GetMousePosition();
+
+                    // Set the target to match, so that the camera maps the
+                    // world space point under the cursor to the screen space
+                    // point under the cursor at any zoom
+                    camera.target = mouseWorldPos;
+                }
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                    // Zoom increment
+                    float deltaX = GetMouseDelta().x;
+                    float scaleFactor = 1.0f + (0.01f * fabsf(deltaX));
+                    if (deltaX < 0)
+                        scaleFactor = 1.0f / scaleFactor;
+                    camera.zoom =
+                        Clamp(camera.zoom * scaleFactor, 0.125f, 64.0f);
+                }
             }
         }
-        else
-        {
-            // Zoom based on mouse left click
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                // Get the world point that is under the mouse
-                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
 
-                // Set the offset to where the mouse is
-                camera.offset = GetMousePosition();
-
-                // Set the target to match, so that the camera maps the world space point 
-                // under the cursor to the screen space point under the cursor at any zoom
-                camera.target = mouseWorldPos;
-            }
-            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            {
-                // Zoom increment
-                float deltaX = GetMouseDelta().x;
-                float scaleFactor = 1.0f + (0.01f*fabsf(deltaX));
-                if (deltaX < 0) scaleFactor = 1.0f/scaleFactor;
-                camera.zoom = Clamp(camera.zoom*scaleFactor, 0.125f, 64.0f);
-            }
-        }
-        }
-
+        SetShaderValue(shader, minRangeLoc, (const void *)&minRange,
+                       SHADER_UNIFORM_FLOAT);
+        SetShaderValue(shader, maxRangeLoc, (const void *)&maxRange,
+                       SHADER_UNIFORM_FLOAT);
 
         BeginDrawing();
         ClearBackground(BLACK);
 
+        BeginTextureMode(screen);
+        DrawRectangle(0, 0, WIDTH, HEIGHT, BLACK);
+        EndTextureMode();
+
         BeginMode2D(camera);
 
+        BeginShaderMode(shader);
         DrawTexture(screen.texture, 0, 0, WHITE);
-
-        // Draw a reference circle
-        // DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 50, MAROON);
+        EndShaderMode();
 
         EndMode2D();
         EndDrawing();
@@ -169,5 +127,7 @@ int main() {
         }
     }
 
+    UnloadShader(shader);
+    UnloadRenderTexture(screen);
     CloseWindow();
 }
---
+++ mandelbrot/shader.glsl
@@ -0,0 +1,63 @@
+#version 460 core
+
+out vec4 fragColor;
+
+uniform vec2 resolution;
+uniform float minRange;
+uniform float maxRange;
+uniform int iterations;
+
+// both functions stolen from somewhere idk where tho
+vec3 hsv2rgb(vec3 hue) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(hue.xxx + K.xyz) * 6.0 - K.www);
+    return hue.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hue.y);
+}
+
+float map(float value, float inMin, float inMax, float outMin, float outMax) {
+    return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+void main()
+{
+    float WIDTH = resolution.x;
+    float HEIGHT = resolution.y;
+
+    float a = map(gl_FragCoord.x, 0, WIDTH, minRange, maxRange);
+    float b = map(gl_FragCoord.y, 0, HEIGHT, minRange, maxRange);
+
+    const float ca = a;
+    const float cb = b;
+
+    int counter = 0;
+
+    while (counter < iterations) {
+        const float aa = a * a - b * b;
+        const float bb = 2 * a * b;
+
+        a = aa + ca;
+        b = bb + cb;
+
+        counter++;
+
+        if (abs(a + b) > 16) { // assuming 16 as infinity
+            break;
+        }
+    }
+
+    if (counter < iterations) {
+        float quotient = float(counter) / float(iterations);
+        float c = clamp(quotient, 0.0, 1.0);
+
+        if (quotient > 0.5) {
+            // Close to the mandelbrot set the color changes
+            // from green to white
+            fragColor = vec4(c, 255, c, 1.0);
+        } else {
+            // Far away it changes from black to green
+            fragColor = vec4(0, c, 0, 1.0);
+        }
+    } else if (counter == iterations) {
+        fragColor = vec4(0.0, 0, 0, 1.0);
+    }
+}
