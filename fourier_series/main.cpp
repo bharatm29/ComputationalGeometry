5244e9cf6122f62ce6038a0510f35d57acbf1e3c
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2025-07-27 12:23:31 +0530

    Fourier - draw cartesian plane

--- fourier_series/main.cpp
+++ fourier_series/main.cpp
@@ -26,29 +26,49 @@ int main() {
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(GetColor(0x403d3d00));
+
+        DrawText("Fourier Series", (WIDTH / 2.0) - (MeasureText("Fourier Series", 30) / 2.0), 100, 30, WHITE);
+
+        double rad = 100.0 * (4 / (1 * PI)); // radius of the big circle
+
+        { // drawing coordinate plane
+            #if 1
+            float cartx = centerx + wave_translate;
+            float carty = centery + rad;
+
+            DrawLine(cartx, carty, cartx, centery - rad, WHITE);
+            DrawLine(cartx, carty, WIDTH, carty, WHITE);
+
+            DrawTriangle({.x = cartx, .y = (float)(centery - rad)}, {.x = cartx - 5, .y = (float)(centery - rad) + 10}, {.x = cartx + 5, .y = (float)(centery - rad) + 10}, WHITE);
+            DrawTriangle({.x = WIDTH, .y = carty}, {.x = WIDTH - 10, .y = carty - 5}, {.x = WIDTH - 10, .y = carty + 5}, WHITE);
+            #endif
+        }
 
         double x = centerx;
         double y = centery;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 5; i++) {
             const int n = i * 2 + 1;
             double prevx = x;
             double prevy = y;
 
-            double rad = 100.0 * (4 / (n * PI));
+            rad = 100.0 * (4 / (n * PI));
             x += rad * cos(n * angle);
             y += rad * sin(n * angle);
 
             DrawCircleLines(prevx, prevy, rad, GetColor(0x80808080));
             DrawLine(prevx, prevy, x, y, BLUE);
+            DrawCircle(x, y, 2, WHITE);
         }
 
-        DrawLine(x, y, centerx + wave_translate, coords.front(), YELLOW);
+        float pointx = centerx + wave_translate;
+        float pointy = coords.front();
+        DrawLine(x, y, pointx, pointy, ORANGE);
+        // DrawCircle(centerx + wave_translate, coords.front(), 2, WHITE);
+        DrawTriangle({.x = pointx, .y = pointy}, {.x = pointx - 10, .y = pointy - 5}, {.x = pointx - 10, .y = pointy + 5}, WHITE);
 
         coords.push_front(y);
-
-        EndDrawing();
 
         angle += 0.01;
         if (angle > 360.0) {
@@ -56,7 +76,6 @@ int main() {
         }
 
         int xx = 0;
-
         vector<Vector2> points;
         for (const auto yy : coords) {
             float nx = xx + centerx + wave_translate;
@@ -67,9 +86,11 @@ int main() {
 
         DrawLineStrip(points.data(), points.size(), RED);
 
-        if (coords.size() > 500) {
+        if (coords.size() > 500) { // just remove out of window points
             coords.pop_back();
         }
+
+        EndDrawing();
     }
 
     CloseWindow();
